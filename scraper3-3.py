ad44cd4d52d113d1b96b772b44a4d7e1e4cb415d
mk920sd <mk920sd@gmail.com>
2022-05-18 11:08:43 +0800

    3.3批次爬取靜態網頁的資料

--- scraper3-3.py
+++ scraper3-3.py
@@ -5,4 +5,10 @@ url = "https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt"
 res = requests.get(url)
 
 soup = BeautifulSoup(res.text, 'lxml')
-print(soup)
+titles = soup.find_all(
+    'span', {'class': 'lx-stream-post__header-text gs-u-align-middle'})
+title_list = []
+for title in titles:
+    title_list.append(title.getText())
+
+print(title_list)
